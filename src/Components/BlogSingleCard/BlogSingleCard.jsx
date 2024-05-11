f61d9516f750ef3290d253b4a95746c0805a3277
Joy <github.joysaha@gmail.com>
2024-05-11 08:53:24 +0600

    using tanstack query and fetch data and show data

---
+++ src/Components/BlogSingleCard/BlogSingleCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const BlogSingleCard = ({item}) => {
+    const {_id, title, image_url, category, short_description} = item
+
+  return (
+    <div>
+      <div className="card w-full h-full bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <span>{category}</span>
+          <p>{short_description}</p>
+          <div className="card-actions items-center justify-between">
+            <button className="btn btn-primary btn-sm rounded-2xl">WishList</button>
+            <button className="btn btn-primary btn-sm rounded-2xl">Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogSingleCard;
--- src/Components/RecentBlog/RecentBlog.jsx
+++ src/Components/RecentBlog/RecentBlog.jsx
@@ -1,14 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import BlogSingleCard from '../BlogSingleCard/BlogSingleCard';
+import { Link } from 'react-router-dom';
 
 const RecentBlog = () => {
+    
+    const {isPending, data: users} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/blogposts');
+            return res.json();
+        }
+    })
+
+    if(isPending){
+        return(<div className="text-center mt-20">
+        <span className="loading  loading-spinner loading-lg"></span>
+      </div>)
+    }
+
+
     return (
         <div className='max-w-6xl mx-auto'>
             <div className='my-10'>
                 <h2 className='text-4xl font-bold'>Recent Blogs</h2>
                 <p>This is where you can get a recent blog which is added by recently from our users. and these blogs are most popular movies data. you can enjoy read blog</p>
             </div>
-            <div>
-
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    users.slice(0, 6).map(item => <BlogSingleCard key={item._id} item={item}></BlogSingleCard>)
+                }
+            </div>
+            <div className='my-6 text-center'>
+                <Link to={"/allblogs"} className='btn btn-accent'>All Blogs</Link>
             </div>
         </div>
     );
