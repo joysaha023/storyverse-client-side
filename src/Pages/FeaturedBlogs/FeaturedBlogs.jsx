8b75b164ccf7f753a9ba6f4eacdcc25fe91869fb
Joy <github.joysaha@gmail.com>
2024-05-12 13:41:45 +0600

    featured page fetch data and create react table data and show data

--- src/Pages/FeaturedBlogs/FeaturedBlogs.jsx
+++ src/Pages/FeaturedBlogs/FeaturedBlogs.jsx
@@ -1,29 +1,65 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import DataTable from "react-data-table-component";
+
+
+const column = [
+  {
+    name: "No",
+    cell: (row, index) => index + 1,
+    sortable: true
+  },
+  {
+    name: "Blog Title",
+    selector: (row) => row.title,
+    sortable: true
+  },
+  {
+    name: "Blog Owner",
+    selector: (row) => row.name,
+    sortable: true
+  },
+  {
+    name: "Owner Photo",
+    selector: (row) => (
+      <img
+        className="rounded-full"
+        width={50}
+        height={50}
+        src={row.profile_pic}
+      />
+    ),
+  },
+  //   {
+  //     name: "Owner Photo",
+  //     selector: (row) =>    <Link to={`/BlogDetails/${data._id}`} className="btn btn-primary btn-sm rounded-2xl">Read More</Link>,
+  //   },
+];
 
 const FeaturedBlogs = () => {
+  const { isPending, data } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/featuredblog");
+      return res.json();
+    },
+  });
 
-    const {isPending, data} = useQuery({
-        queryKey: ['users'],
-        queryFn: async () => {
-            const res = await fetch('http://localhost:5000/featuredblog');
-            return res.json();
-        }
-    })
-
-    if(isPending){
-        return(<div className="text-center mt-20">
-        <span className="loading  loading-spinner loading-lg"></span>
-      </div>)
-    }
-
-   console.log(data)
-
+  if (isPending) {
     return (
-        <div className='max-w-6xl mx-auto'>
-            <h2>Featured Blog</h2>
-        </div>
+      <div className="text-center mt-20">
+        <span className="loading  loading-spinner loading-lg"></span>
+      </div>
     );
+  }
+
+  console.log(data);
+
+  return (
+    <div className="m-6 border-2">
+      <DataTable columns={column} data={data}></DataTable>
+    </div>
+  );
 };
 
 export default FeaturedBlogs;
