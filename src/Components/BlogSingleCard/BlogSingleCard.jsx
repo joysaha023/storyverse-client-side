d0e37f028d2a5bed0d8afca4e2d681303fe95c06
Joy <github.joysaha@gmail.com>
2024-05-13 22:03:55 +0600

    new funcitionality added

--- src/Components/BlogSingleCard/BlogSingleCard.jsx
+++ src/Components/BlogSingleCard/BlogSingleCard.jsx
@@ -2,25 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-const BlogSingleCard = ({item}) => {
-    const {_id, title, image_url, category, short_description} = item
+const BlogSingleCard = ({ item }) => {
+  const { _id, title, image_url, category, short_description } = item;
+
+  const {handlewish} = () => {
+    
+  }
 
   return (
     <div>
-      <motion.div animate={{ x: 0, scale: 1 }} initial={{scale:0}} transition={{delay: 0.5, type: "spring", bounce: 30}} className="card w-full h-full bg-[#ECF0F1] ">
+      <motion.div
+        animate={{ x: 0, scale: 1 }}
+        initial={{ scale: 0 }}
+        transition={{ delay: 0.5, type: "spring", bounce: 30 }}
+        className="card w-full h-full bg-[#ECF0F1] "
+      >
         <figure>
-          <img
-            src={image_url}
-            alt="Shoes"
-          />
+          <img src={image_url} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <span>{category}</span>
           <p>{short_description}</p>
           <div className="card-actions items-center justify-between">
-            <button className="btn bg-[#2C3E50] text-white hover:text-black btn-sm md:btn-md rounded-full">WishList</button>
-            <Link to={`/BlogDetails/${_id}`} className="btn bg-[#3498DB] text-white hover:text-black btn-sm md:btn-md rounded-full">Read More</Link>
+            <button onClick={handlewish} className="btn bg-[#2C3E50] text-white hover:text-black btn-sm md:btn-md rounded-full">
+              WishList
+            </button>
+            <Link
+              to={`/BlogDetails/${_id}`}
+              className="btn bg-[#3498DB] text-white hover:text-black btn-sm md:btn-md rounded-full"
+            >
+              Read More
+            </Link>
           </div>
         </div>
       </motion.div>
--- src/Pages/BlogDetails/BlogDetails.jsx
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -1,15 +1,14 @@
-
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import CardComments from "../../Components/CardComments/CardComments";
 import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const BlogDetails = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
   const [item, setItems] = useState({});
   const [comments, setComments] = useState([]);
   const { id } = useParams();
-  
 
   useEffect(() => {
     fetch(`http://localhost:5000/blogdetails/${id}`)
@@ -25,73 +24,90 @@ const BlogDetails = () => {
     fetch(`http://localhost:5000/comments/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        setComments(data)
+        setComments(data);
       });
   }, [id, commentSubmit]);
 
-
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    commentSubmit()
+    commentSubmit();
     const form = e.target;
     const coment = form.comment.value;
     const blogid = item._id;
     const userName = user?.displayName;
     const userimg = user?.photoURL;
-    const commentData = {coment, blogid, userName, userimg}
-   
+    const commentData = { coment, blogid, userName, userimg };
 
     fetch("http://localhost:5000/commentpost", {
       method: "POST",
       headers: {
-          "content-type" : "application/json",
+        "content-type": "application/json",
       },
-      body: JSON.stringify(commentData)
-  })
-  .then((res) => res.json())
-  .then((data) => {
-      console.log(data)
-      
-  })
-  }
-  
-  
+      body: JSON.stringify(commentData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        Swal.fire({
+          title: "Thanks For Comment",
+          text: "Your comment is valuable for us.",
+          icon: "success",
+        });
+        console.log(data);
+      });
+  };
 
   return (
     <div className="max-w-6xl mx-auto">
       <div className="card my-5 w-full rounded-none bg-base-100 shadow-none  ">
         <figure>
-          <img
-            src={item.image_url}
-            alt="Shoes"
-          />
+          <img src={item.image_url} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title text-2xl font-bold">{item.title}</h2>
-          <p><span className="font-semibold text-lg">Category:</span> {item.category}</p>
+          <p>
+            <span className="font-semibold text-lg">Category:</span>{" "}
+            {item.category}
+          </p>
           <p className="flex items-center gap-1">{item.short_description}</p>
           <p>{item.long_description}</p>
+          <div>
+            {user.email === item.email ? (
+              <Link to={`/updateblog/${item._id}`} className="btn bg-[#2C3E50] text-white hover:text-black btn-sm md:btn-md rounded-full">Update</Link>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
       <div>
-          <h2 className="text-xl font-semibold">Comments:</h2>
-          <div>
-            {
-              comments.map((data) => <CardComments key={data._id} data={data}></CardComments>)
-            }
-          </div>
+        <h2 className="text-xl font-semibold">Comments:</h2>
+        <div>
+          {comments.map((data) => (
+            <CardComments key={data._id} data={data}></CardComments>
+          ))}
+        </div>
       </div>
       <div className="">
         <h2 className="text-xl font-semibold">Write Comment here: </h2>
-      <form onSubmit={handleSubmit}>
-      <label className="flex flex-col md:flex-row  mt-8 w-full md:w-3/5  gap-2">
-      <textarea name="comment" className="textarea w-full textarea-bordered" placeholder={user.email === item.email ? "Can not comment on own blog" : "Write Comment"}></textarea>
-          <button disabled={user.email === item.email} className="btn rounded-md text-white hover:text-black bg-[#0077be]">
-            Comment
-          </button>
-        </label>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <label className="flex flex-col md:flex-row  mt-8 w-full md:w-3/5  gap-2">
+            <textarea
+              name="comment"
+              className="textarea w-full textarea-bordered"
+              placeholder={
+                user.email === item.email
+                  ? "Can not comment on own blog"
+                  : "Write Comment"
+              }
+            ></textarea>
+            <button
+              disabled={user.email === item.email}
+              className="btn rounded-md text-white hover:text-black bg-[#0077be]"
+            >
+              Comment
+            </button>
+          </label>
+        </form>
       </div>
     </div>
   );
